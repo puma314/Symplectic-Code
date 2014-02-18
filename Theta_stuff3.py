c5e37c578fb2c586b70339be9f556551c4d85e46
Roger Van Peski <rogervanpeski@gmail.com>
2014-02-18 12:59:09 -0500

    New version with compartmentalized find_thetaset to avoid memory problems

--- Theta_stuff3.py
+++ Theta_stuff3.py
@@ -2,7 +2,7 @@
 #Created to examine interesting properties of the Theta set of raising operators
 from pylab import *
 from numpy import *
-
+from copy import *
 
 def copy_my_list(l): #this is a deep copy, since for some reason the regular deep copy wasn't working...
     newlist = []
@@ -28,7 +28,7 @@ def make_tuple(differences):
     l = l[::-1] #reverse list
     return l
 
-def reachable_partitions(partition,k):
+def reachable_partitions(partition,k,d):
     '''gives every partition reachable by something in Theta(partition)'''
     S = [partition]
     n = len(partition)
@@ -38,9 +38,13 @@ def reachable_partitions(partition,k):
         for partition1 in S:
             new_reachables = []
             for i in range(n-1):
+##                print partition1
+##                print 'done'
+##                print type(partition1[i+1])
+##                print type(k)
                 if partition1[i] == partition1[i+1] + k:
                     newlist = copy_my_list(partition1)
-                    newlist[i] -= 1; newlist[i+1] += 1
+                    newlist[i] -= d; newlist[i+1] += d
                     new_reachables.append(newlist)
             for reachable in new_reachables:
                 if reachable not in S:
@@ -71,16 +75,34 @@ def efficient_reachable_partitions(partition,k,d):
         if level == newlevel:
             break
         level = newlevel
-        counter += 1
- #       print str(newlevel)
+        newlevel = remove_duplicates(newlevel)
     S = remove_duplicates(S)
     return S
+
+def efficient_reachable_partitions(partition,k,d):
+    n = len(partition)
+    level = [partition]
+    S = []
+    counter = 0
+    while True:
+        S += level
+        newlevel = []
+        for p in level:
+            newlevel += adjacent_reachables(p,k,d)
+        if level == newlevel:
+            break
+        level = newlevel
+        newlevel = remove_duplicates(newlevel)
+    S = remove_duplicates(S)
+    return S
+
+
 
                     
 def vectors(partition,k,d):
     '''returns the vector versions of all possible
     raising operators in Theta(partition)'''
-    S = efficient_reachable_partitions(partition,k,d)
+    S = reachable_partitions(partition,k,d)
     newset = []
     for s in S:
         newset.append(list(array(s) - array(partition)))
@@ -128,19 +150,41 @@ def parse_thetaset(thetaset):
         print 'The number of theta sets with ' + str(i) + ' elements is ' + str(len(newset))
     return newthetaset
 
-def difference_list(length,k):
+def difference_list(length,k,is_strict): #0 for nonstrict, 1 for strict
     '''gives data structure to loop over all possibilities +1, +2, +3 ... +k'''
+ #   memory_limit = 8 #point in the sequence at which you need to start compartmentalizing
     lists = [[]]
     for i in range(length):
+        make_copies(lists,k-is_strict)
+        for it in range(len(lists)/(k+1-is_strict)):
+            for c in range(is_strict, k+1):
+                lists[it+(c-1)*len(lists)/(k+1-is_strict)].append(c)
+    return lists
+
+def reverse_sublists(l):
+    '''reverses every list in list of lists l'''
+    newlist = []
+    for i in l:
+        newlist.append(deepcopy(i[::-1]))
+    return newlist
+
+def efficient_difference_list(length,k): #incomplete
+    '''a more efficient version that takes recursion into
+    account and only makes ones in which [12] or [length-1,length]
+    is in the raising operators set'''
+    lists = [[k-1]]
+    for i in range(length-2):
         make_copies(lists,k-1)
         for it in range(len(lists)/k):
             for c in range(k):
-                lists[it+c*len(lists)/k].append(c+1) 
+                lists[it+c*len(lists)/k].append(c+1)
+    for l in lists:
+        l.append(k-1)
     return lists
 
 
-def make_partitions(n,k):
-    differences = difference_list(n-1,k)
+def efficient_make_partitions(n,k):
+    differences = efficient_difference_list(n-1,k)
     partitions = []
     for d in differences:
         partition = [0]
@@ -150,16 +194,41 @@ def make_partitions(n,k):
         partitions.append(partition)
     return partitions
 
-def make_nonstrict_partitions(n):
-    differences = nonstrict_difference_list(n-1)
+
+def make_partitions(n,k,is_strict): #1 for strict, 0 for nonstrict
+    memory_limit = 8 #point in the sequence at which you need to start compartmentalizing
+    differences = difference_list(n-1,k,is_strict)
     partitions = []
     for d in differences:
         partition = [0]
         for difference in d:
             partition.append(partition[-1]+difference)
-            partition = partition[::-1]
-            partitions.append(partition)
-    return partitions    
+        partition = partition[::-1]
+        partitions.append(partition)
+    return partitions
+
+def bare_make_partitions(differences):
+    '''takes a list of difference lists, turns them into partitions'''
+    partitions = []
+    for d in differences:
+        partition = [0]
+        for difference in d:
+            partition.append(partition[-1]+difference)
+        partition = partition[::-1]
+        partitions.append(partition)
+    return partitions
+    
+
+##def make_nonstrict_partitions(n,k):
+##    differences = nonstrict_difference_list(k-1)
+##    partitions = []
+##    for d in differences:
+##        partition = [0]
+##        for difference in d:
+##            partition.append(partition[-1]+difference)
+##            partition = partition[::-1]
+##            partitions.append(partition)
+##    return partitions    
 
 
 def standard_thetaset(n):
@@ -185,17 +254,125 @@ def distinct_partitions(n,k,d):
     return good_partitions
         
 
-def find_thetaset(n,k,d):
+def find_thetaset(n,k,d): #set is_strict to 1 or 0 as desired
     '''a more efficient version of find_thetaset (old version).
     n is length of partition, k is difference condition, d is
     raising strength (number of 1s moved).'''
-    partitions = make_partitions(n,k+1)
-    raising_ops = []
-    for p in partitions:
-        raising_ops.append(vectors(p,k,d))
-    raising_ops = remove_duplicates(raising_ops)
-    return raising_ops
+    memory_limit = 5 #the memory limit, after which we have to start chunking
+    raising_ops = [] 
+    if n <= memory_limit:
+        partitions = make_partitions(n,k+1,1)     
+        for p in partitions:
+            raising_ops.append(vectors(p,k,d)) 
+        raising_ops = remove_duplicates(raising_ops)
+        return raising_ops
+    else:
+        olddiff = difference_list(memory_limit-1,k+1,1) #change to 0 for nonstrict
+        newdiff = difference_list(n-memory_limit,k+1,1) #again, change 0 if needed
+#        print olddiff
+#        print newdiff
+        for new in newdiff:
+            newolddiff = []
+            for i in olddiff:
+                for j in newdiff:
+                    newolddiff.append(j+i)
+ #           print newolddiff
+ #           break
+            partitions_temp = bare_make_partitions(newolddiff)
+            for p in partitions_temp:
+                v = vectors(p,k,d)
+                raising_ops.append(v)
+            newolddiff,partitions_temp = [],[]
+#        print raising_ops
+        raising_ops = remove_duplicates(raising_ops)
+        return raising_ops
+        
+            
+            
+        
+        
 
+
+def thetaset_dictionary(n,k,d,olddict):
+    '''makes a dictionary that indexes each partition we used by the
+    thetaset it gives (given as a tuple, since python won't let you use
+    lists as dictionary keys, and takes the dictionary of n-1,k,d as an
+    argument to avoid having to compute things multiple times. No guarantees
+    that this will work for different values of d.'''
+    #make all partitions we need:
+    newpartitions = []
+    newdict = {}
+    for thetaset in olddict:
+ #      thetaset = convert_to_lists(thetasettuple,2)
+        maxdiff_front = 0
+        maxdiff_back = 0
+        for raising_op in thetaset:
+  #          print raising_op
+#            print max(maxdiff_front,raising_op[0])
+            maxdiff_front = max(maxdiff_front,raising_op[0])
+            maxdiff_back = max(maxdiff_back,raising_op[-1])
+        to_add = []
+        for partition in olddict[thetaset]:           
+            for i in range(k+1,-maxdiff_front,-1):
+                to_add.append([partition[0]+i] + partition)
+            for i in range(k-1,k-maxdiff_back-1,-1):
+                to_add.append(partition + [partition[-1]-i])
+ #           print to_add
+        newpartitions += to_add
+    #whew, now they're made
+    #make the new dictionary:
+ #   print newpartitions
+    for p in newpartitions:
+        t = convert_to_tuples(vectors(p,k,d),2)
+        if t in newdict:
+            newdict[t].append(p)
+        else:
+            newdict[t] = [p]
+    return newdict
+        
+def convert_to_lists(oldT,d):
+    '''takes a tuple of tuples etc. of depth d (d nestings),
+    and gives it as a list of lists of lists etc.'''
+    if d == 1:
+        return list(oldT)
+    else:
+        T = list(oldT)
+        for i in range(len(T)):
+            T[i] = convert_to_lists(T[i],d-1)
+    return T
+
+
+    
+def convert_to_tuples(oldL,d):
+    '''takes a list of lists of lists etc. of depth d,
+    and converts to tuple of tuples of tuples.'''
+    L = deepcopy(oldL)
+    if d == 1: #list contains no lists
+        return tuple(L)
+    else:
+        for i in range(len(L)):
+            L[i] = convert_to_tuples(L[i],d-1)
+    L = tuple(L)
+    return L
+            
+
+def efficient_find_thetaset(n,k,d,oldthetasuperset):
+    '''takes advantage of recursive nature of thetasets, and
+    take the thetaset on length n-1 as an argument'''
+    partitions = efficient_make_partitions(n,k+1)
+    newset = []
+    for thetaset in oldthetasuperset:
+        front_concatenate = []
+        back_concatenate = []
+        for r in thetaset:
+            front_concatenate.append([0] + r)
+            back_concatenate.append(r + [0])
+        newset.append(back_concatenate)
+        newset.append(front_concatenate)
+ #   for p in partitions: #incomplete
+
+
+    
 def partition_checker(m,n,k,d):
     '''finds all 'ghost partitions' of length m, considering the n
     last parts, with omegaset condition difference of k and
@@ -208,12 +385,29 @@ def partition_checker(m,n,k,d):
             redundant_partitions.append(p)
     return redundant_partitions
         
-def thetaset_sequence(k,n):
+def thetaset_sequence(k,d,n):
     '''defines thetaset procedure for difference k, and tests
     size of thetasets up to length n to output a sequence'''
     for i in range(1,n+1):
-        print str(len(find_thetaset(i,k,1)))
+        print str(len(find_thetaset(i,k,d)))
     return
+
+def thetadict_sequence(k,d,n):
+    '''will keep making theta dictionaries, printing their order,
+    and adding them to a list which can be accessed at will.'''
+    #make the first dictionary:
+    initial_partitions = []
+    for i in range(k+1):
+        initial_partitions.append([i])
+    dictlist = [{convert_to_tuples([[0]],2):initial_partitions}]
+    for it in range(2,n):
+        newsuperset = thetaset_dictionary(it,k,d,dictlist[-1])
+        dictlist.append(newsuperset)
+        print len(newsuperset)
+    return dictlist
+
+#def thetaset_sequencev2(k,d,n):
+    
 
 def fuckyouUma(n,k,d,partition):
     partitions = distinct_partitions(n,k,d)
